4c545c0794dfc9659fae8a35f9fcd225dd35a3bb
Namankumar587 <nk3250744@gmail.com>
2026-06-08 00:30:41 +0530

    intital commit

--- prog39.java
+++ prog39.java
@@ -8,5 +8,19 @@
 import java.util.Scanner;
 
 public class prog39 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }   
 }
