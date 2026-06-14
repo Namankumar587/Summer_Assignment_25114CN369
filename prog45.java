9f740968ea54827b325af1e59d16cb222c1e7cab
Namankumar587 <nk3250744@gmail.com>
2026-06-15 00:09:20 +0530

    intital commit

---
+++ prog45.java
@@ -0,0 +1,27 @@
+//Write a program to Write function for palindrome.
+
+
+public class prog45 {
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String word = "racecar";
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+}
