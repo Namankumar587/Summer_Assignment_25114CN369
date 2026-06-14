6baca321c61946c4201f854e5c2955569394e5c7
Namankumar587 <nk3250744@gmail.com>
2026-06-14 11:44:18 +0530

    intital commit

--- prog40.java
+++ prog40.java
@@ -1,12 +1,32 @@
 //Write a program to Print character pyramid.
-    A
-   ABA
-  ABCBA
- ABCDCBA
-ABCDEDCBA
+     A
+    ABA
+   ABCBA
+  ABCDCBA
+ ABCDEDCBA
+
+
+import java.util.Scanner;
 
 public class prog40 {
-    
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                char ch = (char) ('A' + k - 1);
+                System.out.print(ch);
+            }
+            for (int l = i - 1; l >= 1; l--) {
+                char ch = (char) ('A' + l - 1);
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
 }
