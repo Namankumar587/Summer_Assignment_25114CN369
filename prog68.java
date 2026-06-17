80727f8597cfda7961ff8b50ee16775c3c1cbc88
Namankumar587 <nk3250744@gmail.com>
2026-06-17 13:58:22 +0530

    intital commit

---
+++ prog68.java
@@ -0,0 +1,38 @@
+//Write a program to Find common elements.
+
+import java.util.Scanner;
+
+
+public class prog68 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the first array: ");
+        int n1 = sc.nextInt();
+        
+        int[] arr1 = new int[n1];
+        System.out.println("Enter the elements of the first array: ");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        
+        System.out.println("Enter the number of elements in the second array: ");
+        int n2 = sc.nextInt();
+        
+        int[] arr2 = new int[n2];
+        System.out.println("Enter the elements of the second array: ");
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        
+        System.out.println("Common elements in both arrays: ");
+        for (int num1 : arr1) {
+            for (int num2 : arr2) {
+                if (num1 == num2) {
+                    System.out.print(num1 + " ");
+                    break;
+                }
+            }
+        }
+    }
+    
+}
