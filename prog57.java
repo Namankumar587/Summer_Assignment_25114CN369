8d33503c5268e73e6c451e12455d6c9756c1b2a3
Namankumar587 <nk3250744@gmail.com>
2026-06-17 13:36:16 +0530

    intital commit

---
+++ prog57.java
@@ -0,0 +1,23 @@
+//Write a program to Reverse array.
+
+import java.util.Scanner;
+
+public class prog57 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.println("Reversed array: ");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+}
