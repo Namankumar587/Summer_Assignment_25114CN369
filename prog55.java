708cc9f098cddbf63d7474655185aff4f76a4a9e
Namankumar587 <nk3250744@gmail.com>
2026-06-17 13:33:08 +0530

    intital commit

---
+++ prog55.java
@@ -0,0 +1,35 @@
+//Write a program to Second largest element.
+
+import java.util.Scanner;
+
+public class prog55 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+        
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest element in the array.");
+        } else {
+            System.out.println("The second largest element in the array is: " + secondLargest);
+        }
+    }
+}
