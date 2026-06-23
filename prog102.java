e4241d88a00ec922cc967ca9fe51373551930d2c
Namankumar587 <nk3250744@gmail.com>
2026-06-23 12:16:04 +0530

    intital commit

---
+++ prog102.java
@@ -0,0 +1,18 @@
+//Write a program to Create voting eligibility system.
+
+import java.util.Scanner;
+
+public class prog102 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+    
+}
